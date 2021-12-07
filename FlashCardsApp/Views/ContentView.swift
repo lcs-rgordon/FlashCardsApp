88405d6baa7dcb617d9647bf7d95387eecb27481
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 14:48:47 -0500

    Now have the "another" button working to select a different question.

--- FlashCardsApp/Views/ContentView.swift
+++ FlashCardsApp/Views/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
-    var currentCard: Card = listOfCards.randomElement()!
+    @State var currentCard: Card = listOfCards.randomElement()!
     
     // MARK: Computed properties
     var body: some View {
@@ -34,7 +34,7 @@ struct ContentView: View {
 
             // Input
             Button(action: {
-                print("Button was pressed")
+                currentCard = listOfCards.randomElement()!
             }, label: {
                 Text("Another")
             })
