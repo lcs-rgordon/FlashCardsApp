f37176d0165202b086e2683798e1431652367084
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 14:35:40 -0500

    Half of the question interface has been built in a static manner.

--- FlashCardsApp/Views/ContentView.swift
+++ FlashCardsApp/Views/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("What's \"powerhouse\" of a cell?")
+                .font(.largeTitle)
+            
+            Button(action: {
+                print("Button was pressed")
+            }, label: {
+                Text("Check")
+            })
+            .buttonStyle(.bordered)
+            
+            Spacer()
+
+        }
+        .navigationTitle("Flash Cards")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
