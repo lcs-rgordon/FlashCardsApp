3ae67f2f0f640441569f4dbebeaf31dc75687d6f
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 14:28:17 -0500

    Got a few questions added to our list of cards and defined the structure.

---
+++ FlashCardsApp/Model/Card.swift
@@ -0,0 +1,34 @@
+//
+//  Card.swift
+//  FlashCardsApp
+//
+//  Created by Russell Gordon on 2021-12-07.
+//
+
+import Foundation
+
+struct Card {
+    
+    let question: String
+    let answer: String
+    
+}
+
+let listOfCards = [
+
+    Card(question: "What's \"powerhouse\" of a cell?",
+         answer: "mitochondria")
+    
+    ,
+
+    Card(question: "What is the largest bone in your body?",
+         answer: "femur")
+    
+    ,
+
+    Card(question: "What is acceleration due to gravity?",
+         answer: "9.80 m/s squared")
+    
+    ,
+
+]
