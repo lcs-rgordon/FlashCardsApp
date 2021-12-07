558fd89dfc9deed575332700f42cb44e9e50833a
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 14:16:19 -0500

    Initial Commit

---
+++ FlashCardsApp/FlashCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsAppApp.swift
+//  FlashCardsApp
+//
+//  Created by Russell Gordon on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
